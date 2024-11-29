02b4de92f97171cf55de66bf8b2493855a6d36d7
Elisabeth Pfeiffer <elisabeth@Elisabeths-MacBook-Air.local>
2024-11-29 21:09:18 +0000

    client.sh modified to use arrays

--- client.sh
+++ client.sh
@@ -4,13 +4,15 @@ if [ ! $# -eq 1 ]; then
   exit 1
 fi
 
-id = $1
+id=$1
 echo "Accepted Commands: {create|add|post|display}"
 while true; do #loops infinitely
-  read request #reads user input
-  command=$(cut -d ' ' -f1 < $request)
-  arguments=$(cut -d ' ' -f1 --complement < $request)
-  echo $command $id $arguments > $id_pipe #this is supposed to send the arguments through a pipe to be received from server.sh, I'm not sure if this works???
+  read -a arguments #reads user input
+  command=${arguments[0]}
+  command[1]=$id
+  unset arguments[0]
+  input+=( ${command[@]} ${arguments[@]} )
+  echo $input[@]} > $id_pipe #this is supposed to send the arguments through a pipe to be received from server.sh, I'm not sure if this works???
   #smth that reads the output of server.sh and gives user-friendly output accordingly
 done
  
