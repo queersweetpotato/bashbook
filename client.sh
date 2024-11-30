3077c4c61ebf65230c3d3450e998c7d25dcc5b7e
Elisabeth Pfeiffer <elisabeth@Elisabeths-MacBook-Air.local>
2024-11-30 13:05:06 +0000

    commented client.sh and server.sh

--- client.sh
+++ client.sh
@@ -5,17 +5,17 @@ if [ ! $# -eq 1 ]; then
 fi
 
 id=$1
-mkfifo ty_pipe
+mkfifo user_pipe
 
 echo "Accepted Commands: {create|add|post|display}"
 while true; do #loops infinitely
-  read -a arguments #reads user input
-  command=${arguments[0]}
-  command[1]=$id
-  unset arguments[0]
-  input+=( ${command[@]} ${arguments[@]} )
-  echo ${input[@]} > ty_pipe  
-  read response < server_pipe
+  read -a arguments #reads user input as an array
+  command=${arguments[0]} #create array command that stores the innputted comand
+  command[1]=$id #add the id as the next field in the array
+  unset arguments[0] #remove the command from the array arguments
+  input+=( ${command[@]} ${arguments[@]} ) #create new array combining command and arguments
+  echo ${input[@]} > user_pipe  #send this to server.sh through user_pipe
+  read response < server_pipe #retrieve the output from server.sh
   echo $response
 done
-rm ty_pipe 
+ 
--- server.sh
+++ server.sh
@@ -3,15 +3,14 @@
 mkfifo server_pipe
 
 while true; do #loops infinitely  
-  read -a arguments < ty_pipe
+  read -a arguments < user_pipe #receive input as an array from client.sh
   command=${arguments[0]}
   unset arguments[0]
   #splits the request to the server to a command used
-  #in the switch case and arguments to be passed to the selected
-  #command.
+  #in the switch case and arguments to be passed to the selected command.
   case "$command" in
     create)
-      ./create.sh ${arguments[@]} > server_pipe 
+      ./create.sh ${arguments[@]} > server_pipe #send output to client.sh
       ;;
     add)
       ./add_friend.sh ${arguments[@]} > server_pipe
