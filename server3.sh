3cfa84f76fc999f19aed2f516f0524b4df263347
tywithoutanswers <tymekx0@gmail.com>
2024-12-01 16:46:14 +0000

    Added extra echo to server3 for debugging purposes

--- server3.sh
+++ server3.sh
@@ -9,20 +9,25 @@ mkfifo user_pipe
 
 while true; do #loops infinitely  
   read -a arguments < user_pipe #receive input as an array from client.sh
+  echo Recieved message: $arguments
   command=${arguments[0]}
   unset arguments[0]
   #splits the request to the server to a command used in the switch case and arguments to be passed to the selected command.
   case "$command" in
     create)
+      echo "recieved create command"
       ./create.sh ${arguments[@]} > server_pipe #send output to client.sh
       ;;
     add)
+      echo "recieved add command"
       ./add_friend.sh ${arguments[@]} > server_pipe
       ;;
     post)
+      echo "recieved post command"
       ./post_messages.sh ${arguments[@]} > server_pipe
       ;;
     display)
+      echo "recieved display command"
       ./display_wall.sh ${arguments[@]} > server_pipe
       ;;
     *)
