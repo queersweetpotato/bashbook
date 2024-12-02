aa2a38da179a49f3d30f5bdfa4a33b9089700674
Elisabeth Pfeiffer <elisabeth@Elisabeths-MacBook-Air.local>
2024-12-02 16:21:29 +0000

    added locks to post and display

--- display_wall.sh
+++ display_wall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 id=$1
+
+trap "./release.sh '$id/friends.txt'" EXIT #lock is released on exist so that none remain acquired if program is closed before finishing
+
 if [ $# -lt 1 ] || [ $# -gt 1 ]; then
   exit 1
 fi
@@ -9,6 +12,8 @@ if [ ! -d $id ]; then
   echo "nok: user '$id' does not exist"
   exit 1
 fi
+
+./acquire.sh "$id/wall.txt"
 
 echo "start_of_file"
 cat $id/wall.txt 
--- post_messages.sh
+++ post_messages.sh
@@ -3,6 +3,8 @@
 sender=$1
 receiver=$2
 
+trap "./release.sh '$receiver/friends.txt'; ./release.sh '$receiver/walls.txt'" EXIT #lock is released on exist so that none remain acquired if program is closed before finishing
+
 if [ $# -lt 3 ]; then
   exit 1
 fi
@@ -17,10 +19,14 @@ if [ ! -d $receiver ]; then
   exit 1
 fi
 
+./acquire.sh "$receiver/friends.txt"
+
 if ! grep "^$sender" "$receiver"/friends.txt > /dev/null; then
   echo nok: users $sender and $receiver are not friends
   exit 1
 fi
+
+./acquire.sh "$receiver/wall.txt"
 
 shift 2
 echo "$sender: $@" >> $receiver/wall.txt
