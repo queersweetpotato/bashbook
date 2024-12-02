fbc79fdeed91e542c2a1aa39c84b652eea024216
tywithoutanswers <tymekx0@gmail.com>
2024-12-02 13:37:06 +0000

    built up scripts for locks test case, race conditions are happenin YIPEE

--- locktest/sillyclient.sh
+++ locktest/sillyclient.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #queries sillyserver to run simple commands
-#
+if [[ $# -eq 0 ]] then
+	exit 1
+fi
 
-for i in $(seq 1 $#);
+for i in "$@"
 do
-	echo ${args[$i]} > sillyserver_pipe
+	echo $i > sillyserver_pipe
 done
--- locktest/sillyserver.sh
+++ locktest/sillyserver.sh
@@ -2,12 +2,12 @@
 
 #runs many instances of command.sh (which all manipulate resource.txt) in parallel
 
-trap "rm -f sillyserver_pipe" EXIT
-if [[ ! -p sillyserver_pipe ]]
+trap "rm -f sillyserver_pipe ; cat resource.txt" EXIT
+if [[ ! -p sillyserver_pipe ]] then
 	mkfifo sillyserver_pipe
 fi
 
 while true; do
 	read input < sillyserver_pipe
-	./command.sh $input
+	./command.sh $input & # runs command.sh instances asynchronously
 done
